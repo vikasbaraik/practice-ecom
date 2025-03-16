ef0506838229f6a526b4a717bd28964e67a228a7
vikasbaraik <vbaraik3@gmail.com>
2025-03-17 02:05:31 +0530

    add to cart functionality added

--- script.js
+++ script.js
@@ -69,3 +69,13 @@ function displayProducts(products) {
         productsContainer.appendChild(productCard);
     });
 }
+
+function addToCart(product) {
+    const existingItem = cart.find(item => item.id === product.id);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({ ...product, quantity: 1 });
+    }
+}
