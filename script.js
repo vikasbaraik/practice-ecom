261a29284dec19a0e0d53f150610394afa1534b3
vikasbaraik <vbaraik3@gmail.com>
2025-03-17 01:01:09 +0530

    starter code

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Problem statement
+Using the HTML code given in the scaffold, implement a fully functional online shopping 
+which should use an API for all the products.
+
+Requirements:-
+1- Fetch all the products using the following API: https://dummyjson.com/products
+2- The JSON you get from the API contain detail of each product, such as title, price, rating, images, etc.\
+3- Each product fetched from the API should be displayed on the screen.
+4- The API gives you more than one image for each product. The product should be displayed in such a way that it should have the option to toggle the images to the next or previous images.
+5- Each product should have the corresponding "Add to Cart" option, which should be able to add the corresponding product to the cart.
+6- There should be a cart button at the top page on click, of which the content of the page could be toggled from the cart section to all product sections and vice-versa.
+7- The cart section should display all the products that have been added by the user with the quantity.
+8- There should be an option to decrease the quantity of the product or remove the product from the cart.
+9- In the cart section, there should be a "checkout" button on click of which an alert should be shown with the total price to be paid by the user.
+*/
